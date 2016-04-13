2578363d6031383ca5065721db90ec49a436099e
kstro96 <alejokstro2011@hotmail.com>
2016-04-13 15:24:22 -0500

    NombreComparator

---
+++ src/co/edu/javeriana/ambulancias/negocio/NombreComparator.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package co.edu.javeriana.ambulancias.negocio;
+
+import java.util.Comparator;
+
+/**
+ * @author Pablo Ariza y Alejandro Castro
+ *
+ */
+public class NombreComparator implements Comparator<String>{
+
+	public int compare(String nombre1, String nombre2){
+		return nombre1.compareTo(nombre2);
+	}
+}
