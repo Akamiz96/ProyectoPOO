fe3dfb2f175882cfc7bf1252d4201fd7dc11e30e
kstro96 <alejokstro2011@hotmail.com>
2016-05-04 14:29:23 -0500

    Enums

---
+++ src/co/edu/javeriana/ambulancias/negocio/EstadoServicio.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package co.edu.javeriana.ambulancias.negocio;
+
+/**
+ * @author Pablo Ariza y Alejandro Castro
+ *
+ */
+public enum EstadoServicio {
+	NO_ASIGNADO,ASIGNADO,FINALIZADO;
+	
+}
---
+++ src/co/edu/javeriana/ambulancias/negocio/TipoServicio.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package co.edu.javeriana.ambulancias.negocio;
+
+/**
+ * @author Pablo Ariza y Alejandro Castro
+ *
+ */
+public enum TipoServicio {
+	DOMICILIO,URGENCIA,EMERGENCIA;
+}
